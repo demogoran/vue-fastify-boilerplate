973210874bf58abe3ebae2737a549e5a87dcad3b
Dem Demoff <demogoran@gmail.com>
2019-04-27 16:36:21 +0300

    deploy

--- client/dist/main.js
+++ client/dist/main.js
@@ -228,13 +228,13 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_3__["default"]({
     path: '/',
     name: 'main',
     component: function component() {
-      return Promise.all(/*! import() */[__webpack_require__.e(0), __webpack_require__.e(1)]).then(__webpack_require__.bind(null, /*! ../templates/MainPage.vue */ "./client/templates/MainPage.vue"));
+      return __webpack_require__.e(/*! import() */ 3).then(__webpack_require__.bind(null, /*! ../templates/MainPage.vue */ "./client/templates/MainPage.vue"));
     }
   }, {
     path: '/login',
     name: 'login',
     component: function component() {
-      return Promise.all(/*! import() */[__webpack_require__.e(0), __webpack_require__.e(2)]).then(__webpack_require__.bind(null, /*! ../templates/LoginPage.vue */ "./client/templates/LoginPage.vue"));
+      return __webpack_require__.e(/*! import() */ 0).then(__webpack_require__.bind(null, /*! ../templates/LoginPage.vue */ "./client/templates/LoginPage.vue"));
     }
   }]
 });
