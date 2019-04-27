766e1fc727b5e27422db6227fd0801dee91faed4
Dem Demoff <demogoran@gmail.com>
2019-04-27 16:53:38 +0300

    deploy

---
+++ webpack.prod.config.js
@@ -0,0 +1,75 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const Dotenv = require('dotenv-webpack');
+
+
+const config = {
+  entry: './client/js/index.js',
+  output: {
+    filename: '[name].js',
+    publicPath: 'dist/',
+    chunkFilename: '[name].bundle.js',
+    path: `${__dirname}/client/dist`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer(),
+              ],
+            },
+          },
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // Use the full build
+    }
+  },
+  watch: false,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+    new VueLoaderPlugin(),
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: false,
+      }),
+    ],
+  }
+};
+
+module.exports = config;
